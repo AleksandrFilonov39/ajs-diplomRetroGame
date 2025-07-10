a036303e4e1997069dac488c0b4a511d183f7848
Filonov <ABFilonov@yandex.ru>
2025-07-10 23:59:20 +0200

    page

--- dist/main.js
+++ dist/main.js
@@ -247,7 +247,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 				}
 /******/ 			}
 /******/ 		};
-/******/ 	})();
+/******/ 	})();  
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
 /******/ 	(() => {
